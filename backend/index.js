929894428dc6a007a1c5a96fe4a8b3d493270f14
Sumit-Kushwaha62 <sumitkushwaha99375@gmail.com>
2026-06-15 08:25:17 +0530

    fix cors for production

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,14 @@ require('dotenv').config();
 const humanizeRouter = require('./routes/humanize');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://humanizer-frontend-ji1t.onrender.com'
+  ]
+}));
+
 app.use(express.json());
 
 app.use('/api/humanize', humanizeRouter);
